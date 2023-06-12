b0712f959a74f54e6669523145a8b6259aba2ea9
SEBASTIAN ALEXANDER CARRIZO ZULETA <sebastian.carrizo03@inacapmail.cl>
2023-06-12 18:06:31 -0400

    Se agregó un botón para cambiar el título

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const boton = document.getElementById('cambiar_titulo')
+
+boton.addEventListener('click', () => document.getElementById('titulo').innerHTML = "<h1>Ya no estamos aprendiendo control de versiones 😔</h1>")
